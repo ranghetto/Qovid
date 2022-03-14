7cf21d33ee6b369b39a74a2353a61a0443705d04
Matteo Rango <rango.matteo@icloud.com>
2022-03-14 19:13:23 +0100

    Aggiunta widget simulazione (vuoto)

---
+++ src/SimulationView.cpp
@@ -0,0 +1,8 @@
+#include "SimulationView.h"
+
+SimulationView::SimulationView(QWidget *parent) : QWidget(parent) {
+  setFixedSize(500, 500);
+  resize(500, 500);
+}
+
+// void SimulationView::paintEvent(QPaintEvent *event) {}

fb8e5f5b77369d1bca587dcd82c0c35eadecfa72
Matteo Rango <rango.matteo@icloud.com>
2022-03-22 17:09:32 +0100

    Add comments to headers
    https://github.com/ranghetto/Qovid/issues/9

--- src/PersonModel.h
+++ src/PersonModel.h
@@ -10,18 +10,27 @@ class ActionAbstractModel;
 class PersonModel : public QObject {
   Q_OBJECT
 public:
+  // position: initial position, does not check boundaries, if any;
+  // speed: initial speed as a QVector2D;
+  // acceleration: initial acceleration as QVector2D;
   explicit PersonModel(QPointF position, QVector2D speed = QVector2D(0, 0),
                        QVector2D acceleration = QVector2D(0, 0));
 
+  // method to implement the way the object is drawn
   virtual void draw(QPainter &painter);
+
+  // perform given action, if any;
   void doAction();
+
+  // check speedlimit and adapt it, must be used after setSpeed() to affect
+  // speed correctly
+  void limitSpeed(float speedLimit);
 
   // setters
   void setPosition(QPointF position);
   void setSpeed(QVector2D speed);
   void setAcceleration(QVector2D acceleration);
   void setAction(ActionAbstractModel &action);
-  void limitSpeed(float speedLimit);
 
   // getters
   QPointF getPosition() const;

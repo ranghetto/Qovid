feec2826e21b1e2b0b60008b949d53ace3216655
Matteo Rango <rango.matteo@icloud.com>
2022-03-28 18:56:19 +0200

    Aggiunta behavior "infetta" base

---
+++ src/AI/InfectActorsInRange.cpp
@@ -0,0 +1,21 @@
+#include "InfectActorsInRange.h"
+#include "../Controllers/Simulation.h"
+
+InfectActorsInRange::InfectActorsInRange(Actor &actor, int range)
+    : actor_(actor), range_(range) {}
+
+NodeState InfectActorsInRange::evaluate(qint64 deltaTime) {
+
+  QVector<Actor *> people = Simulation::instance().world()->findNearbyActors(
+      actor_.position(), range_);
+
+  if (actor_.healthState() == ActorHealthState::INFECTED) {
+    for (auto p : people) {
+      if (qrand() % 100 < 20)
+        p->setHealthState(ActorHealthState::INFECTED);
+    }
+  }
+
+  state_ = NodeState::RUNNING;
+  return state_;
+}

6b655ea52afa78fc56ebc8bc438b944d1358fbc9
FakeDaemon <oselieroantonio@gmail.com>
2022-04-08 16:01:54 +0200

    Eliminated useless controller

--- src/Controllers/ViewController.cpp
+++
@@ -1,7 +0,0 @@
-#include "ViewController.h"
-
-ViewController::ViewController(QObject *parent): main_window_(new MainWindow), sim_controller(new Simulation){}
-
-void ViewController::show(){
-    main_window_->show();
-}

bc3eaaf77c46c4ed2dcb8db04cd75f0b1566342e
FakeDaemon <oselieroantonio@gmail.com>
2022-04-09 17:38:18 +0200

    new input field, getter

--- src/Views/InputWidget.cpp
+++ src/Views/InputWidget.cpp
@@ -7,15 +7,17 @@ InputWidget::InputWidget(QWidget *parent) : QWidget(parent) {
   QVBoxLayout *layout = new QVBoxLayout(this);
   // label
   label_population = new QLabel(this);
-  label_infection = new QLabel(this);
+  label_infection_range = new QLabel(this);
   label_infection_rate = new QLabel(this);
   label_death_rate = new QLabel(this);
   label_time = new QLabel(this);
+  label_recover = new QLabel(this);
   // input field
   input_population = new QSpinBox(this);
-  input_infection = new QSpinBox(this);
+  input_infection_range = new QSpinBox(this);
   input_infection_rate = new QSpinBox(this);
   input_death_rate = new QSpinBox(this);
+  input_time_toRecover = new QSpinBox(this);
   // radio menu
   sim_duration_30s = new QRadioButton("30 secondi", this);
   sim_duration_1m = new QRadioButton("1 minuto", this);
@@ -25,28 +27,33 @@ InputWidget::InputWidget(QWidget *parent) : QWidget(parent) {
   start_sim = new QPushButton("Inizia Simulazione", this);
   // label text
   label_population->setText("Grandezza della popolazione");
-  label_infection->setText("Raggio del contagio");
+  label_infection_range->setText("Raggio del contagio");
   label_infection_rate->setText("Probabilità di essere contagiati");
   label_death_rate->setText("Probabilità di morte dell'infetto");
   label_time->setText("Durata simulazione");
+  label_recover->setText("Durata infezione");
   // SpinBox max min setting
   input_population->setMaximum(300);
   input_population->setMinimum(2);
-  input_infection->setMaximum(100);
-  input_infection->setMinimum(0);
+  input_infection_range->setMaximum(100);
+  input_infection_range->setMinimum(0);
   input_infection_rate->setMaximum(100);
   input_infection_rate->setMinimum(0);
   input_death_rate->setMaximum(100);
   input_death_rate->setMinimum(0);
+  input_time_toRecover->setMaximum(10000);
+  input_time_toRecover->setMinimum(500);
   // adding widget to layout
   layout->addWidget(label_population);
   layout->addWidget(input_population);
-  layout->addWidget(label_infection);
-  layout->addWidget(input_infection);
+  layout->addWidget(label_infection_range);
+  layout->addWidget(input_infection_range);
   layout->addWidget(label_infection_rate);
   layout->addWidget(input_infection_rate);
   layout->addWidget(label_death_rate);
   layout->addWidget(input_death_rate);
+  layout->addWidget(label_recover);
+  layout->addWidget(input_time_toRecover);
   layout->addWidget(label_time);
   layout->addWidget(sim_duration_30s);
   layout->addWidget(sim_duration_1m);
@@ -71,3 +78,22 @@ void InputWidget::setSimulation(Simulation *controller){
 int InputWidget::getPopulation(){
   return input_population->value();
 }
+
+int InputWidget::getInfectionRange(){
+  return input_infection_range->value();
+}
+
+int InputWidget::getInfectionRate(){
+  return input_infection_rate->value();
+}
+
+int InputWidget::getDeathRate(){
+  return input_death_rate->value();
+}
+
+int InputWidget::getTimeRecover(){
+  return input_time_toRecover->value();
+}
+
+
+
--- src/Views/InputWidget.h
+++ src/Views/InputWidget.h
@@ -18,11 +18,15 @@ public:
     //setters & getters
     void setSimulation(Simulation*);
     int getPopulation();
+    int getInfectionRange();
+    int getInfectionRate();
+    int getDeathRate();
+    int getTimeRecover();
 private:
-  QLabel *label_population, *label_infection, *label_infection_rate,
-      *label_death_rate, *label_time;
-  QSpinBox *input_population, *input_infection, *input_infection_rate,
-      *input_death_rate;
+  QLabel *label_population, *label_infection_range, *label_infection_rate,
+      *label_death_rate, *label_time, *label_recover;
+  QSpinBox *input_population, *input_infection_range, *input_infection_rate,
+      *input_death_rate, *input_time_toRecover;
   QRadioButton *sim_duration_30s, *sim_duration_1m, *sim_duration_3m,
       *sim_duration_complete;
   QPushButton *start_sim;

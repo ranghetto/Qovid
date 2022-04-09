24987a7920bd230e986fa584bd6eac6aac95f1a0
FakeDaemon <oselieroantonio@gmail.com>
2022-04-09 19:00:30 +0200

    settable infected population by view

--- src/Controllers/World.cpp
+++ src/Controllers/World.cpp
@@ -3,21 +3,23 @@
 #include <QTime>
 #include <QVector2D>
 
-World::World(int population, int infection_range, int infection_rate, int death_rate, int time_toRecover) {
+World::World(int population, int infection_range, int infection_rate, int death_rate, int time_toRecover, int initial_infect) {
   qsrand(QTime::currentTime().msec());
 
-  for (int j = 0; j < population; j++) {
+  for (int j = 0; j < population; j++) 
+  {
     QVector<QVector2D> v;
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 6; i++) 
+    {
       v.append(QVector2D(qrand() % 500, qrand() % 500));
     }
-    //DEATH_RATE==SURVIVE_RATE MUST FIX
     Actor *a = new Actor(v[5], 0.2f, ActorHealthState::HEALTHY,
                          {v[0], v[1], v[2], v[3], v[4]}, 2000, infection_range,
                         time_toRecover, death_rate, infection_rate);
-    if (qrand() % 100 < 2)
+    if(population-initial_infect<=j)
+    {
       a->setHealthState(ActorHealthState::INFECTED);
-
+    }
     addEntity(*a);
   }
 }

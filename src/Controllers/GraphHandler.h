1ad01bc667516d1bc30e6e315784c75303da3fde
Matteo Rango <rango.matteo@icloud.com>
2022-05-30 10:36:37 +0200

    Fix warnings

--- src/Controllers/GraphHandler.h
+++ src/Controllers/GraphHandler.h
@@ -1,32 +1,32 @@
 #ifndef GRAPHHANDLER_H
 #define GRAPHHANDLER_H
-#include <QObject>
-#include <QListWidgetItem>
-#include <QDir>
-#include <QDebug>
-#include "../Views/ContainerWidget.h"
-#include "../Views/OldSimulation.h"
 #include "../Loggers/ActorsLogger.h"
 #include "../Views/Charts/AreaChart.h"
 #include "../Views/Charts/BarChart.h"
 #include "../Views/Charts/PieChart.h"
 #include "../Views/Charts/ScatterChart.h"
 #include "../Views/ChartsWidget.h"
+#include "../Views/ContainerWidget.h"
+#include "../Views/OldSimulation.h"
+#include <QDebug>
+#include <QDir>
+#include <QListWidgetItem>
+#include <QObject>
 
-class GraphHandler: public QObject{
-    Q_OBJECT
-    public:
-    GraphHandler(QObject *parent = nullptr);
-    void setContainerWidgets(ContainerWidget*);
-    QDir* getCurrentDirectory();
-    void PopulateOldSimulation(QDir*);
-    public slots:
-    void showGraph(QListWidgetItem*);
-    private:
-    ContainerWidget* container_;
-    OldSimulation* simulation_;
-    QDir *directory_;
+class GraphHandler : public QObject {
+  Q_OBJECT
+public:
+  GraphHandler(QObject *parent = nullptr);
+  void setContainerWidgets(ContainerWidget *);
+  QDir *getCurrentDirectory();
+  void PopulateOldSimulation(QDir *);
+public slots:
+  void showGraph(QListWidgetItem *);
+
+private:
+  ContainerWidget *container_;
+  OldSimulation *simulation_;
+  QDir *directory_;
 };
-
 
 #endif
